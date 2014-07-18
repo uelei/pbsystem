e2eac2b0a28cfda003ff6757fc705c2cccfdc162
uelei <github@uelei.com>
2014-07-18 15:49:58 -0300

    grafic adjust

--- js/graficos.js
+++ js/graficos.js
@@ -5,8 +5,8 @@ function gera () {
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Vendedor');
   data.addColumn('number', 'Valor');
-
-  var d = $("#inputMes").val(); 
+ var wid = document.getElementById('funcgrafico').offsetWidth;
+  var d = $("#inputMes").val();
   $.post( "http://pbs.piubol.com.br/index.php/venda/dadosdograficovendas", { inputmes : d })
     .done(function(dat){
       var  obj = jQuery.parseJSON(dat);
@@ -16,7 +16,7 @@ function gera () {
         data.addRow([ obj[x].username, parseFloat(obj[x].ts) ]);
       }
       var s = "Total das Vendas : R$ "+ t.toFixed(2).toString();
-      var options = {'title':s,is3D: true,'width':600,'height':400};
+      var options = {'title':s,is3D: true,'width':wid,'height':wid};
      var  chart = new google.visualization.PieChart(document.getElementById('funcgrafico')).draw(data, options);
     });
 }
@@ -30,12 +30,12 @@ function gera () {
 
 function gera2 () {
   var t = 0.0;
-  
+  var widc = document.getElementById('funcgraficoc').offsetWidth;
   var datames = new google.visualization.DataTable();
   datames.addColumn('string', 'Vendedor');
   datames.addColumn('number', 'Valor');
 
-  var d = $("#inputMes").val(); 
+  var d = $("#inputMes").val();
   $.post( "http://pbs.piubol.com.br/index.php/venda/dadosdograficovendasmm", { inputmes : d })
     .done(function(dat){
       var  obj = jQuery.parseJSON(dat);
@@ -45,7 +45,7 @@ function gera2 () {
         datames.addRow([ obj[x].username, parseFloat(obj[x].ts) ]);
       }
       var s = "Total das Vendas no Mes : R$ "+ t.toFixed(2).toString();
-      var options = {'title':s,is3D: true,'width':600,'height':400};
+      var options = {'title':s,is3D: true,'width': widc,'height':widc};
      var  chartmes = new google.visualization.PieChart(document.getElementById('funcgraficoc')).draw(datames, options);
     });
 }
@@ -63,7 +63,8 @@ function barra () {
   var da = new google.visualization.DataTable();
   da.addColumn('string', 'Dias');
   da.addColumn('number', 'Sales');
-var d = $("#inputMes").val(); 
+  var d = $("#inputMes").val();
+  var widd = document.getElementById('daychart').offsetWidth;
   $.post( "http://pbs.piubol.com.br/index.php/venda/dadosdograficovendasd", { inputmes : d })
     .done(function(dat){
        var  obj = jQuery.parseJSON(dat);
@@ -75,7 +76,7 @@ var d = $("#inputMes").val();
 // var da = google.visualization.arrayToDataTable(dat);
           var s = "Vendas do Mes : R$ "+ t.toFixed(2).toString();
         var options = {
-          title: s,'width':1100,'height':600,
+          title: s,'width':widd,'height':widd*0.4,
           vAxis : {minValue: 0 },
           hAxis: {title: 'Dias', titleTextStyle: {color: 'red'}}
         };
@@ -100,19 +101,20 @@ function barra2 () {
   var daa = new google.visualization.DataTable();
   daa.addColumn('string', 'Dias');
   daa.addColumn('number', 'Sales');
-var d = $("#inputMes").val(); 
+var d = $("#inputMes").val();
+ var widm = document.getElementById('mchart').offsetWidth;
   $.post( "http://pbs.piubol.com.br/index.php/venda/dadosdograficovendasm", { inputmes : d })
     .done(function(dados){
        var  obj = jQuery.parseJSON(dados);
       for(x in obj){
         t = t+ parseFloat(obj[x].ts);
         //addiciona dados ao grafico 
-        daa.addRow([ obj[x].m, parseFloat(obj[x].ts) ]);  
+        daa.addRow([ obj[x].m, parseFloat(obj[x].ts) ]);
         }
 // var da = google.visualization.arrayToDataTable(dat);
           var s = "Vendas do Mes : R$ "+ t.toFixed(2).toString();
         var options = {
-          title: s,'width':1100,'height':600,
+          title: s,'width':widm,'height':widm*0.4,
           vAxis : {minValue: 0 },
           hAxis: {title: 'Dias', titleTextStyle: {color: 'red'}}
         };
